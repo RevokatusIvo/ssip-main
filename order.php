2d446d7a237f559ee73e238720796834eced8601
Praditya_Abyan <pradityaabyan27@gmail.com>
2024-04-25 03:40:51 +0700

    crud blm jd

--- order.php
+++ order.php
@@ -68,6 +68,31 @@
             cursor: pointer;
             border-radius: 20px;
         }
+
+        /* Sticky footer styles */
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding-bottom: 100px;
+            /* Height of the footer */
+        }
+
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #25b3ff;
+            color: white;
+            text-align: center;
+            height: 40px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
     </style>
 </head>
 
@@ -77,7 +102,6 @@
             <div class="menu-column">
                 <img src="./image/image1.png" alt="./image/image 1">
                 <input type="number" class="number-column">
-                <button class="submit-button">Submit</button>
             </div>
             <div class="menu-column-text">
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum libero, vero, quos magni commodi repudiandae quaerat, repellendus autem tenetur dignissimos veniam quasi saepe nulla temporibus sit quas laudantium cum corrupti.</p>
@@ -87,7 +111,6 @@
             <div class="menu-column">
                 <img src="./image/image2.png" alt="./image/image 2">
                 <input type="number" class="number-column">
-                <button class="submit-button">Submit</button>
             </div>
             <div class="menu-column-text">
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quisquam molestiae fuga ad animi quaerat tempora excepturi earum, assumenda nostrum, nesciunt fugit, nihil aspernatur dolor! Facilis accusantium obcaecati at architecto.</p>
@@ -97,7 +120,6 @@
             <div class="menu-column">
                 <img src="./image/image3.png" alt="./image/image 3">
                 <input type="number" class="number-column">
-                <button class="submit-button">Submit</button>
             </div>
             <div class="menu-column-text">
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatem excepturi facilis qui dicta dolorum quod porro laboriosam dolor quidem, corrupti pariatur reiciendis consequatur alias nam aliquid nulla perferendis! Atque?</p>
@@ -107,7 +129,6 @@
             <div class="menu-column">
                 <img src="./image/image4.png" alt="./image/image 4">
                 <input type="number" class="number-column">
-                <button class="submit-button">Submit</button>
             </div>
             <div class="menu-column-text">
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non libero excepturi dolor quibusdam similique autem, alias laudantium doloremque impedit idsapiente qui, doloribus totam, fugit dicta sunt omnis cum error!</p>
@@ -115,9 +136,8 @@
         </div>
         <div class="menu-row">
             <div class="menu-column">
-                <img src="./image/image5.png" alt="./image/image 5">
+                <img src="./image/image1.png" alt="./image/image 5">
                 <input type="number" class="number-column">
-                <button class="submit-button">Submit</button>
             </div>
             <div class="menu-column-text">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perspiciatis quam odit facilis accusantium dolores explicabo reiciendis animi nulla amet, in vel qui ullam velit corrupti? Exercitationem magni necessitatibus iure?</p>
@@ -125,11 +145,16 @@
         </div>
     </div>
 
+    <div class="footer">
+        <form method="post" action="order.php">
+            <button type="submit" class="submit-button">Submit</button>
+        </form>
+    </div>
+
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get form data
         $quantity = $_POST['quantity'];
-        $menu_id = $_POST['menu_id'];
 
         // Connect to database
         $conn = mysqli_connect("localhost", "username", "password", "database");
@@ -140,7 +165,7 @@
         }
 
         // Insert data into database
-        $sql = "INSERT INTO orders (menu_id, quantity) VALUES ('$menu_id', '$quantity')";
+        $sql = "INSERT INTO orders (quantity) VALUES ('$quantity')";
         if (mysqli_query($conn, $sql)) {
             echo "Data inserted successfully";
         } else {
